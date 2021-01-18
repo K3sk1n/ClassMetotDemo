d8f20534831e77999e5f6e2e3089bd6023bda57f
K3sk1n <77547569+K3sk1n@users.noreply.github.com>
2021-01-18 14:34:43 +0200

    Add files via upload

---
+++ Musterimanager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musterimanager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler Eklendiniz! "  + "\n" + "Detaylariniz:" + "\n" + "Adi:" + musteri.Adi + "\n" + "Soyadi:" + musteri.Soyadi + "\n" + "Id:" + musteri.Id+ "\n" + "TC Kimlik no:" + musteri.Tckimlik);
+            Console.WriteLine("--------------------------------------------------------------------");
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Adi: " + musteri.Adi  + "\n" + "Soyadi: " + musteri.Soyadi  + "\n" + "Id:" + musteri.Id  + "\n" + "TC Kimlik no: " + musteri.Tckimlik);
+            Console.WriteLine("--------------------------------------------------------------------");
+        }
+
+        public void Silme(Musteri musteri) 
+        {
+            Console.WriteLine(musteri.Id +" Id numarali "  +  musteri.Adi  + musteri.Soyadi + " Musteri Bilgisi Silindi");
+            Console.WriteLine("--------------------------------------------------------------------");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Mehmet Taha";
+            musteri1.Soyadi = "Kaya";
+            musteri1.Id = 1234;
+            musteri1.Tckimlik = 12345678901;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Engin";
+            musteri2.Soyadi = "Demirog";
+            musteri2.Id = 4321;
+            musteri2.Tckimlik = 10987654321;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Ahmet";
+            musteri3.Soyadi = "Karatas";
+            musteri3.Id = 2314;
+            musteri3.Tckimlik = 13579124680;
+
+            Musterimanager musterimanager = new Musterimanager();
+            musterimanager.Ekle(musteri1);
+            musterimanager.Silme(musteri2);
+            musterimanager.Listele(musteri3);
+        }
+    }
+}
